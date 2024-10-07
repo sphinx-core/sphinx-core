77d688c999ce784e1e523868f75a32724d77efae
Kusuma <newsphinx29@gmail.com>
2024-10-07 22:28:38 +0700

    Update SHA3.h

--- src/crypto/Swifftx/SHA3.h
+++ src/crypto/Swifftx/SHA3.h
@@ -20,8 +20,6 @@
 // SWIFFTX-related constants portion
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-typedef unsigned char BitSequence;
-
 extern const BitSequence SALT_VALUE;
 extern const BitSequence HAIFA_IV_256;
 
