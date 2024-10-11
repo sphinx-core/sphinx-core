dd061f7d54f8e16706490967564bf2777fabfaa4
Kusuma <newsphinx29@gmail.com>
2024-10-11 09:15:25 +0700

    Update denomination.go

--- src/params/denomination.go
+++ src/params/denomination.go
@@ -39,9 +39,31 @@ const (
 // 1 SPX = 1e18 nSPX, and 1 gSPX = 1e9 nSPX.
 //
 // Example of conversions:
-//
-// To convert gSPX to nSPX, you can multiply the value in gSPX by gSPX multiplier:
-// new(big.Int).Mul(valueInGSPX, big.NewInt(params.gSPX))
-//
-// To convert SPX to nSPX, multiply the value in SPX by the SPX multiplier:
-// new(big.Int).Mul(valueInSPX, big.NewInt(params.SPX))
+
+// 1. Converting gSPX to nSPX:
+// To convert an amount in gSPX to nSPX, multiply the value by the gSPX multiplier (1e9).
+// For example, if you have 5 gSPX, the conversion to nSPX would be:
+// valueInGSPX := new(big.Int).SetInt64(5)                // 5 gSPX
+// valueInNSPX := new(big.Int).Mul(valueInGSPX, big.NewInt(gSPX)) // 5 * 1e9 = 5e9 nSPX
+// Result: valueInNSPX is now equal to 5,000,000,000 nSPX
+
+// 2. Converting SPX to nSPX:
+// To convert an amount in SPX to nSPX, multiply the value by the SPX multiplier (1e18).
+// For example, if you have 2 SPX, the conversion to nSPX would be:
+// valueInSPX := new(big.Int).SetInt64(2)                 // 2 SPX
+// valueInNSPX2 := new(big.Int).Mul(valueInSPX, big.NewInt(SPX)) // 2 * 1e18 = 2e18 nSPX
+// Result: valueInNSPX2 is now equal to 2,000,000,000,000,000,000 nSPX
+
+// 3. Converting nSPX to gSPX:
+// To convert an amount in nSPX back to gSPX, divide the value by the gSPX multiplier (1e9).
+// For example, if you have 1,000,000,000 nSPX, the conversion to gSPX would be:s
+// valueInNSPX3 := new(big.Int).SetInt64(1000000000)          // 1,000,000,000 nSPX
+// valueInGSPX2 := new(big.Int).Div(valueInNSPX3, big.NewInt(gSPX)) // 1,000,000,000 / 1e9 = 1 gSPX
+// Result: valueInGSPX2 is now equal to 1 gSPX
+
+// 4. Converting nSPX to SPX:
+// To convert an amount in nSPX to SPX, divide the value by the SPX multiplier (1e18).
+// For example, if you have 1,000,000,000,000,000,000 nSPX, the conversion to SPX would be:
+// valueInNSPX4 := new(big.Int).SetInt64(1000000000000000000) // 1,000,000,000,000,000,000 nSPX
+// valueInSPX2 := new(big.Int).Div(valueInNSPX4, big.NewInt(SPX)) // 1,000,000,000,000,000,000 / 1e18 = 1 SPX
+// Result: valueInSPX2 is now equal to 1 SPX
