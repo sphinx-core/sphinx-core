0e4383ecce1b774f9b6d828796e989976bab3892
Kusuma <newsphinx29@gmail.com>
2024-10-11 09:11:40 +0700

    Update denomination.go

--- src/params/denomination.go
+++ src/params/denomination.go
@@ -22,18 +22,26 @@
 
 package params
 
-// These are the multipliers for SPX denominations.
-// Example: To get the nSPX value of an amount in 'gSPX', use
-//
-//	new(big.Int).Mul(value, big.NewInt(params.gSPX))
+// These are the multipliers for SPX denominations, which help to represent
+// different units of the SPX token in the Sphinx blockchain, similar to how
+// currencies like Bitcoin and Ethereum use smaller units such as satoshis and wei.
+// The base unit is nSPX (nano SPX), which is the smallest denomination, and
+// larger units such as gSPX (giga SPX) and SPX (1 full token) are represented
+// by multiplying nSPX values.
+
 const (
-	nSPX = 1e0  // 1 nSPX
-	gSPX = 1e9  // 1 gSPX = 1e9 nSPX
-	SPX  = 1e18 // 1 SPX = 1e18 nSPX
+	nSPX = 1e0  // 1 nSPX (nano SPX) is the smallest unit of SPX, similar to "wei" in Ethereum.
+	gSPX = 1e9  // 1 gSPX (giga SPX) equals 1e9 nSPX. It's a larger denomination used for easier representation.
+	SPX  = 1e18 // 1 SPX equals 1e18 nSPX, similar to how 1 Ether equals 1e18 wei.
 )
 
-// To convert gSPX to nSPX, use:
+// In the same way that 1 Ether = 1e18 wei, here:
+// 1 SPX = 1e18 nSPX, and 1 gSPX = 1e9 nSPX.
+//
+// Example of conversions:
+//
+// To convert gSPX to nSPX, you can multiply the value in gSPX by gSPX multiplier:
 // new(big.Int).Mul(valueInGSPX, big.NewInt(params.gSPX))
 //
-// To convert SPX to nSPX, use:
+// To convert SPX to nSPX, multiply the value in SPX by the SPX multiplier:
 // new(big.Int).Mul(valueInSPX, big.NewInt(params.SPX))
