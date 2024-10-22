f7331888f195e74306a17f375bd77028b7d49bbb
Kusuma <newsphinx29@gmail.com>
2024-10-22 21:43:55 +0700

    update

--- src/core/spxhash/hash/spxhash.go
+++ src/core/spxhash/hash/spxhash.go
@@ -148,9 +148,9 @@ const (
 	prime32     = 0x9e3779b9         // Example prime constant for 32-bit hash
 	prime64     = 0x9e3779b97f4a7c15 // Example prime constant for 64-bit hash
 	saltSize    = 16                 // Size of salt in bytes
-	memory      = 64 * 1024          // Memory cost (64×1024 = 65536 KB) for Argon2
-	iterations  = 4                  // Number of iterations for Argon2
-	parallelism = 6                  // Number of parallel threads for Argon2
+	memory      = 37 * 1024 * 1024   // Memory cost set to 37 MiB (37 * 1024 * 1024 bytes)
+	iterations  = 1                  // Number of iterations for Argon2id set to 1
+	parallelism = 1                  // Degree of parallelism set to 1
 )
 
 // NewSphinxHash creates a new SphinxHash with a specific bit size for the hash.
