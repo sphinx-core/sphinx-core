f31bb7d45acf80b75c54439e40bad2d53d1b6e1e
Kusuma <newsphinx29@gmail.com>
2024-10-24 16:29:08 +0700

    Create test_passkey.go

---
+++ src/accounts/test_passkey.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	seed "github.com/sphinx-core/sphinx-core/src/accounts/phrase"
+)
+
+func main() {
+	// Generate passphrase and passkey
+	passphrase, base32Passkey, err := seed.GeneratePassphraseAndPasskey()
+	if err != nil {
+		log.Fatalf("Error generating passphrase and passkey: %v", err)
+	}
+
+	// Print the generated passphrase and Base32-encoded passkey
+	fmt.Printf("Generated Passphrase: %s\n", passphrase)
+	fmt.Printf("Base32-Encoded Passkey: %s\n", base32Passkey)
+}
