a3d04b136f0c252a9adf8ef05f04c2ab9940276d
Kusuma <newsphinx29@gmail.com>
2024-10-29 01:28:29 +0700

    update

--- src/core/spxhash/hash/spxhash.go
+++ src/core/spxhash/hash/spxhash.go
@@ -149,7 +149,9 @@ const (
 	prime32  = 0x9e3779b9         // Example prime constant for 32-bit hash
 	prime64  = 0x9e3779b97f4a7c15 // Example prime constant for 64-bit hash
 	saltSize = 16                 // Size of salt in bytes (128 bits = 16 bytes)
-	// This is not recomended setup for Argon memory standard that is required minimum 15Mib memory in allocation
+
+	// Argon2 parameters
+	//Argon memory standard that is required minimum 15Mib memory in allocation
 	memory           = 64 * 1024 // Memory cost set to 64 KiB (64 * 1024 bytes)
 	iterations       = 2         // Number of iterations for Argon2id set to 2
 	parallelism      = 1         // Degree of parallelism set to 1
